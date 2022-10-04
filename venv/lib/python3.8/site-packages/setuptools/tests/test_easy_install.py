00ad9846c4acd4963ebe02a323b73be837f0b3a2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 17:24:57 +0000

    Adding numeric_owner as keyword arguement

--- venv/lib/python3.8/site-packages/setuptools/tests/test_easy_install.py
+++ venv/lib/python3.8/site-packages/setuptools/tests/test_easy_install.py
@@ -555,7 +555,7 @@ class TestSetupRequires:
                             if not is_within_directory(path, member_path):
                                 raise Exception("Attempted Path Traversal in Tar File")
                     
-                        tar.extractall(path, members, numeric_owner) 
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
                         
                     
                     safe_extract(tf, foobar_1_dir)
